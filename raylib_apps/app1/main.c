25df6a52ba88c6b0dabf9e12494a7cd5d2a3f502
LennMesselken <Lenn.Messelken@gmail.com>
2025-02-13 21:54:56 +0100

    Played around a bit with raylib

--- raylib_apps/app1/main.c
+++ raylib_apps/app1/main.c
@@ -1,14 +1,22 @@
 #include "include/raylib.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#define X_SIZE 800
+#define Y_SIZE 600
 
 int main() {
-    InitWindow(800, 600, "Hello Raylib");
+    InitWindow(X_SIZE, Y_SIZE, "Hello World");
+    SetTargetFPS(120);
     while (!WindowShouldClose()) {
-        SetWindowPosition(0,0);
+        
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("This is a test", 350, 280, 20, BLACK);
+        DrawFPS(0,0);
         EndDrawing();
+
+        
     }
     CloseWindow();
     return 0;
