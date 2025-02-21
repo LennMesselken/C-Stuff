0380701bbce5bc8b8630119c436a43456be73bf9
LennMesselken <Lenn.Messelken@gmail.com>
2025-02-21 23:30:58 +0100

    Finished Work on Singly Linked List

--- data_structures_&_algos/data_strcutures/singly_linked_list.c
+++ data_structures_&_algos/data_strcutures/singly_linked_list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct SLL_Node {
     int value;
     struct SLL_Node* next;
@@ -64,16 +65,47 @@ void SLL_append(SLL* List, int value){
     return;
 };
 
-void SLL_contains(){
-
+int SLL_contains(SLL* List, int value){
+    SLL_Node* current = List->head;
+    int i = 0;
+    while(current != NULL){
+        if(current->value == value){
+            return i;
+        } else {
+            i++;
+            current = current->next; 
+        }
+    }
+    return -1;
 };
 
-int SLL_get_length(){
-    return 0;
+int SLL_get_length(SLL* List){
+    if(List->head == NULL){
+        return 0;
+    }
+    int l = 1;
+    SLL_Node* current = List->head->next;
+    while(current != NULL){
+        l++;
+        current = current->next;
+    }
+    return l;
 };
 
-void SLL_remove(){
-
+void SLL_remove(SLL* List, int index){
+    if (index == 0){
+        List->head = List->head->next;
+        return;
+    }
+    SLL_Node* current = List->head;
+    for (int i = 0; i < index -1; i++){
+        if(current == NULL){
+            return;
+        }
+        current = current->next;
+    }
+    current->next = current->next->next;
+    return;
 };
 
 void SLL_print(SLL* List){
@@ -92,7 +124,10 @@ int main(){
     SLL_insert(&test_list, 0, 2);
     SLL_insert(&test_list, 0, 1);
     SLL_insert(&test_list, 1, 5);
+    SLL_append(&test_list, 10);
     SLL_print(&test_list);
+    int x = SLL_contains(&test_list, 111);
+    printf("%d\n", x);
     return 0;
 }
 
