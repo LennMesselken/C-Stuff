dc561a97666beccc268eed7bb71702787d9c21fd
LennMesselken <Lenn.Messelken@gmail.com>
2025-02-24 23:55:42 +0100

    Worked on Insert on BST

--- data_structures_&_algos/data_strcutures/bst.c
+++ data_structures_&_algos/data_strcutures/bst.c
@@ -11,12 +11,71 @@ typedef struct {
     BST_Node* root;
 } BST;
 
+void BST_init(BST* Tree){
+    Tree->root = NULL;
+    return;
+}
+
 BST_Node* BST_create_node(int value){
     BST_Node* newNode = (BST_Node*) malloc(sizeof(BST_Node));
     newNode->value = value;
     newNode->left = NULL;
     newNode->right = NULL;
     return newNode;
+}
+
+void BST_insert(BST* Tree, int value){
+    BST_Node* newNode = BST_create_node(value);
+    if (Tree->root == NULL){
+        Tree->root = newNode;
+        return;
+    }
+    BST_Node* leading, trailing;
+    leading = Tree->root;
+    while (leading != NULL){
+        trailing = *leading;
+        if (value < leading->value){
+            leading = leading->left;
+            continue;
+        } else {
+            leading = leading->right;
+            continue;
+        }
+    }
+
+    if (value < trailing.value){
+        trailing.left = newNode;
+    } else {
+        trailing.right = newNode;
+    }
+    return;
+}
+
+
+
+void BST_print(BST* Tree){
+    BST_Node* current = Tree->root;
+    while(current != NULL){
+        printf("%d >left> ", current->value);
+        current = current->left;
+    }
+    printf("\n");
+    return;
+    
+    
+}
+
+int main(){
+    BST test_tree;
+    BST_init(&test_tree);
+    BST_print(&test_tree);
+    BST_insert(&test_tree, 10);
+    BST_print(&test_tree);
+    BST_insert(&test_tree, 5);
+    BST_print(&test_tree);
+    BST_insert(&test_tree, 2);
+    BST_print(&test_tree);
+
 }
 
 
